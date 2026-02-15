e71b3de53b495b9eb1df05517c396324fd702ec6
mengoerkin <131490088+mengoerkin@users.noreply.github.com>
2026-02-15 19:21:40 +0500

    Update Truck.cs

--- DZ4/Truck.cs
+++ DZ4/Truck.cs
@@ -1,20 +1,21 @@
-public class Truck implements IVehicle {
-
+public class Truck : IVehicle
+{
     private double loadCapacity;
     private int axles;
 
-    public Truck(double loadCapacity, int axles) {
+    public Truck(double loadCapacity, int axles)
+    {
         this.loadCapacity = loadCapacity;
         this.axles = axles;
     }
 
-    @Override
-    public void drive() {
-        System.out.println("Truck with " + loadCapacity + " tons capacity is driving.");
+    public void Drive()
+    {
+        Console.WriteLine($"Truck with {loadCapacity} tons capacity is driving.");
     }
 
-    @Override
-    public void refuel() {
-        System.out.println("Refueling truck.");
+    public void Refuel()
+    {
+        Console.WriteLine("Refueling truck.");
     }
 }
